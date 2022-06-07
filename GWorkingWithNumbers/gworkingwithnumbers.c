4e40eb7fd2640b5c3af9be2baf0810c8db4b9e8a
Lance <Metal_Death_Music@yahoo.com>
2022-06-07 14:11:53 -0400

    GWorkingWithNumbers

---
+++ GWorkingWithNumbers/gworkingwithnumbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    int num = 6;
+
+    printf("%f\n", 8.9);
+
+    printf("%f\n", 5.0 + 4.5);
+
+    printf("%d\n", num);
+
+    printf("%f\n", pow(2, 3));
+
+    printf("%f\n", sqrt(36));
+
+    printf("%f\n", ceil(36.356)); //round up
+    
+    printf("%f\n", floor(36.656)); //round down
+
+    return 0;
+}
