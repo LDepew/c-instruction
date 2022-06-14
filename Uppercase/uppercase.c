43010dee570a62facbebddfe4aaa070b40abb4b0
Lance <Metal_Death_Music@yahoo.com>
2022-06-14 18:06:57 -0400

    Uppercase Update

--- Uppercase/uppercase.c
+++ Uppercase/uppercase.c
@@ -12,7 +12,7 @@ int main(void)
     {
         if(islower(s[i]))
         {
-            printf("%c", s[i] - 32);
+            printf("%c", toupper(s[i]));
         }
         else {
             printf("%c", s[i]);
