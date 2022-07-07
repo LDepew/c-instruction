ed219195990884aa8c0773fdbb7530a68b30bee9
Lance <Metal_Death_Music@yahoo.com>
2022-07-07 16:49:34 -0400

    Address pointer arithmetic

--- Address/address.c
+++ Address/address.c
@@ -4,6 +4,16 @@
 int main(void)
 {
     char *s = "HI!";
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
+
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+    printf("%i\n", *numbers);
+    printf("%i\n", *(numbers + 1));
+    printf("%i\n", *(numbers + 2));
+
+    /*
     char *p = &s[0];
     printf("%p\n", p);
     printf("%p\n", s);
@@ -11,7 +21,7 @@ int main(void)
     printf("%p\n", &s[1]);
     printf("%p\n", &s[2]);
     printf("%p\n", &s[3]);
-
+    */
     /*
     int n = 50;
     int *p = &n;
