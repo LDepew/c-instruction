a1e53410d6240aae8f5462917bf1e2ffe0ddbf5e
Lance <Metal_Death_Music@yahoo.com>
2022-09-29 18:34:07 -0400

    CS50 Link List

--- List/list.c
+++ List/list.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+    //Dynamically allocate array of size 3
     int *list = malloc(3 * sizeof(int));
 
     if (list == NULL)
@@ -10,13 +11,15 @@ int main(void)
         return 1;
     }
 
+    //assign 3 numbers to that array
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
     // Time Passes
 
-    int *tmp = malloc(4 * sizeof(int));
+    //resize old array to be of size 4
+    int *tmp = realloc(list, 4 * sizeof(int));
 
     if (tmp == NULL)
     {
@@ -24,22 +27,26 @@ int main(void)
         return 1;
     }
 
+    /* No Longer Necessary with realloc!
+    //copy numbers of old array into new array
     for (int i = 0; i < 3; i++)
     {
         tmp[i] = list[i];
-    }
+    } */
 
+    //Add 4th number to new array
     tmp[3] = 4;
 
-    free(list);
-
+    //remember new array
     list = tmp;
 
+    //print new array
     for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list[i]);
     }
 
+    //free new array
     free(list);
     return 0;
 }
